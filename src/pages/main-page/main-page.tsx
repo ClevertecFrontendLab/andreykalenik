a504272a83512a8d05b0a4c645c576b60b382818
andreykalenik <61594325+andreykalenik@users.noreply.github.com>
2024-02-12 03:25:37 +0300

    fix: small screen bug

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -11,7 +11,7 @@ import {
   import type { MenuProps } from 'antd';
   import {Layout, Menu, Button} from 'antd'
  
-  import React, { useState } from 'react';
+  import React, { useState} from 'react';
   import { useWindowSize } from 'usehooks-ts'
 
   import { ExitIcon, LogoIcon, LogoSmallIcon } from '@components/project icons';
@@ -68,33 +68,38 @@ export const MainPage: React.FC = () => {
 
     const [collapsed, setCollapsed] = useState(false)
     const  screenWidht = useWindowSize().width
+    
     const mobileBreakpoint = 576
     const padBreakpoint = 992
+    const trigger = screenWidht > mobileBreakpoint
 
+
+    
     return (
       <Layout className={styles.mainLayout}>
         <Sider 
+          style={!trigger ? {zIndex:5, position:'absolute', height:'100%'}: {}}
           className={styles.sider}
           collapsible collapsed={collapsed}
-          width={screenWidht > mobileBreakpoint ? 204 : 106}
-          collapsedWidth={screenWidht > mobileBreakpoint ? 64 : 1}
+          width={trigger ? 204 : 106}
+          collapsedWidth={trigger ? 64 : 1}
           trigger={null}
           onCollapse={value => setCollapsed(value)}>
           <div className={styles.siderLogo}>
-               { screenWidht > mobileBreakpoint ?
+               { trigger ?
                collapsed ?  <LogoSmallIcon/> : <LogoIcon/>:
                collapsed ?  null : <LogoIcon/>
                }
 
           </div>
           <div className={styles.siderMenuWrapper}>
-            <Menu className={styles.siderNav} mode="vertical" items={screenWidht > mobileBreakpoint ? items: itemsMobile} />
-            <Menu className={styles.siderFooter} mode="vertical" items={screenWidht > mobileBreakpoint ? footer: footerMobile} />
+            <Menu className={styles.siderNav} mode="vertical" items={trigger ? items : itemsMobile} />
+            <Menu className={styles.siderFooter} mode="vertical" items={trigger ? footer : footerMobile} />
           </div>
           <Button
                   type='text'
                   onClick={() => setCollapsed(!collapsed)}
-                  data-test-id={screenWidht > mobileBreakpoint ? 'sider-switch' : 'sider-switch-mobile'}
+                  data-test-id={trigger ? 'sider-switch' : 'sider-switch-mobile'}
                   icon={collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}
               />
         </Sider>
