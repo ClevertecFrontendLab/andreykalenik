ade802a0c85762ed631fb57f0f5a83c8df6b6299
andreykalenik <61594325+andreykalenik@users.noreply.github.com>
2024-02-17 15:33:58 +0300

    fix: add data-test-id to EntryPage

--- src/pages/entry-page/EntryPage.tsx
+++ src/pages/entry-page/EntryPage.tsx
@@ -5,74 +5,6 @@ import styles from './EntryPage.module.scss'
 import { LogoIcon } from '@components/project icons'
 import { ServiceBackground } from '@components/service background'
 
-
-const RegistrationForm:React.FC = () =>{
-    const { Link } = Typography;
-    const { useBreakpoint } = Grid;
-    const {sm} = useBreakpoint()
-
-    const onFinish = (values: any) => {
-        console.log('Success:', values);
-      };
-    
-      const onFinishFailed = (errorInfo: any) => {
-        console.log('Failed:', errorInfo);
-      };
-
-    return(
-        <Form
-        name="basic"
-        size='large'
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-        className={styles.form}
-      >
-        <Form.Item
-          label="e-mail:"
-          name="username"
-          className={styles.emailInput}
-          rules={[{required: false, message: 'Please input your username!' }]
-        }
-        >
-          <Input style={{borderRadius:2}}/>
-        </Form.Item>
-  
-        <Form.Item
-          label=""
-          name="password"
-          extra="Пароль не менее 8 символов, с заглавной буквой и цифрой"
-          rules={[{required: false, message: 'Please input your password!' }]}
-        >
-          <Input.Password placeholder='Пароль' style={{borderRadius:2}} />
-        </Form.Item>
-
-        <Form.Item
-          label=""
-          name="password"
-          rules={[{required: false, message: 'Please input your password!' }]}
-        >
-          <Input.Password placeholder='Повторите пароль' style={{borderRadius:2}} />
-        </Form.Item>
-        
-  
-        <Form.Item style={{marginBottom:16}}>
-          <Button type="primary" htmlType="submit" style={sm ? {width:'100%', borderRadius:2}:{width:'100%', borderRadius:2, fontSize:14}}>
-            Войти
-          </Button>
-        </Form.Item>
-
-        <Form.Item>
-          <Button htmlType="submit" style={sm ? {width:'100%', borderRadius:2}:{width:'100%', borderRadius:2, fontSize:14}}>
-          {sm ? <GooglePlusOutlined size={14}/>:null}
-            Войти через Google
-          </Button>
-        </Form.Item>
-      </Form>
-    )
-
-}
 const Login:React.FC = () =>{
   const { Link } = Typography;
   const { useBreakpoint } = Grid;
@@ -100,6 +32,7 @@ const Login:React.FC = () =>{
         label="e-mail:"
         name="username"
         className={styles.emailInput}
+        data-test-id='login-email'
         rules={[{required: false, message: 'Please input your username!' }]
       }
       >
@@ -109,22 +42,37 @@ const Login:React.FC = () =>{
       <Form.Item
         label=""
         name="password"
+        data-test-id='login-password'
         rules={[{required: false, message: 'Please input your password!' }]}
       >
         <Input.Password style={{borderRadius:2}} />
       </Form.Item>
       
       <div className={styles.additionalActions}>
-          <Form.Item name="remember" valuePropName="checked" className={styles.formCheckbox} >
+          <Form.Item 
+            name="remember" 
+            valuePropName="checked" 
+            data-test-id='login-remember'
+            className={styles.formCheckbox} >
           <Checkbox>Запомнить меня</Checkbox>
           </Form.Item>
-          <Link style={sm ? {fontSize:17, fontOpticalSizing:'auto'}: {fontSize:14,fontOpticalSizing:'auto', marginRight:8}} >Забыли пароль?</Link>
+          <Link 
+            data-test-id='login-forgot-button'
+            style={sm ? {fontSize:17, fontOpticalSizing:'auto'}: {fontSize:14,fontOpticalSizing:'auto', marginRight:8}} 
+            >
+              Забыли пароль?
+          </Link>
       </div>
 
 
       <Form.Item style={{marginBottom:16}}>
-        <Button type="primary" htmlType="submit" style={sm ? {width:'100%', borderRadius:2}:{width:'100%', borderRadius:2, fontSize:14}}>
-          Войти
+        <Button 
+          type="primary"
+          htmlType="submit"
+          data-test-id='login-submit-button'
+          style={sm ? {width:'100%', borderRadius:2}:{width:'100%', borderRadius:2, fontSize:14}}
+          >
+            Войти
         </Button>
       </Form.Item>
 
@@ -138,8 +86,6 @@ const Login:React.FC = () =>{
   )
 
 }
-
-
 
 export const EntryPage:React.FC = () =>{
 
