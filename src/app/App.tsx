0e8b9a68fd1940e3318387854f0f44d7ea8a908c
andreykalenik <61594325+andreykalenik@users.noreply.github.com>
2024-02-26 01:31:07 +0300

    refactor:  antd theme as separate file

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,66 +2,18 @@ import  React  from "react";
 import { Provider } from 'react-redux';
 import { Route, Routes, Navigate, Outlet} from 'react-router-dom';
 import { HistoryRouter as Router } from 'redux-first-history/rr6';
-import { MainPage, AuthPage, ResultPage} from './pages';
+import { MainPage, AuthPage, ResultPage} from '../pages';
 import { ConfigProvider } from 'antd';
 import { store, history } from '@redux/configure-store';
-import { ROUTER_PATHS } from './utils/constants';
+import { ROUTER_PATHS } from '../utils/constants';
 import { ConfirmEmail } from "@components/confirm-email";
 import { ChangePassword } from "@components/change-password";
-
+import  { AntTheme  }  from './AntTheme'
 
 
 export const App:React.FC = () =>{
-
-
     return(
-        <ConfigProvider theme={{
-            
-            token:{
-                fontFamily:'Inter',
-                lineHeight: 1.3,
-                colorPrimary:'#2f54eb',
-                colorText:"#262626",
-                colorTextSecondary:"#8C8C8C",
-                colorLink:"#2f54eb",
-                colorErrorText:"#262626"
-            },
-            components:{
-                Card:{
-                    borderRadiusLG:2,
-                    borderRadiusSM:2,
-                    borderRadiusXS:2,
-                    borderRadiusOuter:2,
-                    colorBorder:'none'
-                },
-
-                Input:{
-                    borderRadius:2,
-                    borderRadiusLG:2,
-                    borderRadiusSM:2,
-                    borderRadiusXS:2,
-                    borderRadiusOuter:2,
-                    fontSize:14,
-                    fontSizeLG:14,
-                    fontSizeSM:14,
-                    fontSizeXL:14,
-                    controlHeight:40,
-                    
-                },
-                
-                Button:{
-                    borderRadius:2,
-                    borderRadiusLG:2,
-                    borderRadiusSM:2,
-                    borderRadiusXS:2,
-                    borderRadiusOuter:2,
-                    controlHeight:40,
-                    fontSize:16,
-                },
-            
-            }
-            
-        }}>
+        <ConfigProvider theme={AntTheme}>
             <Provider store={store}>
                 <Router history={history}>
                     <Routes>
