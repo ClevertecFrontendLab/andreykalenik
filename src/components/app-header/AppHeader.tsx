4f0a3319d23e9420238bac02f14e8b0d63371980
andreykalenik <61594325+andreykalenik@users.noreply.github.com>
2024-03-19 14:20:39 +0300

    fix: AppHeader and AppBreadcrumb

--- src/components/app-header/AppHeader.tsx
+++ src/components/app-header/AppHeader.tsx
@@ -13,8 +13,10 @@ export const AppHeader: React.FC = () => {
     const { lg, xs } = useBreakpoint();
 
     const isMainPage = useLocation().pathname === Path.MAIN;
+    const isFeedbackPage = useLocation().pathname === Path.FEEDBACKS;
+
     return (
-        <header className={isMainPage ? styles.header : styles.headerAlternative}>
+        <header className={isMainPage || isFeedbackPage ? styles.header : styles.headerAlternative}>
             {isMainPage ? (
                 <>
                     <p className={styles.headerTitle}>Главная</p>
@@ -25,42 +27,40 @@ export const AppHeader: React.FC = () => {
                         добиться своей <br />
                         мечты!
                     </h1>
-                    <div className={styles.headerActionsWrapper}>
-                        {lg ? (
-                            <Link
-                                style={{
-                                    display: 'flex',
-                                    gap: '10px',
-                                    color: `${Colors.character_light_title_85}`,
-                                }}
-                            >
-                                <SettingOutlined />
-                                Настройки
-                            </Link>
-                        ) : xs ? (
-                            <Button
-                                style={{
-                                    fontSize: 14,
-                                    width: 32,
-                                    height: 32,
-                                    borderRadius: '50%',
-                                    border: `1px solid ${Colors.character_light_border}`,
-                                    background: `${Colors.character_light_sider_background}`,
-                                    padding: '7px 8px',
-                                }}
-                                type='text'
-                                icon={<SettingOutlined />}
-                            />
-                        ) : (
-                            <Link style={{ color: `${Colors.character_light_title_85}` }}>
-                                Настройки
-                            </Link>
-                        )}
-                    </div>
                 </>
             ) : (
                 <AppBreadcrumb />
             )}
+            <div className={styles.headerActionsWrapper}>
+                {lg ? (
+                    <Link
+                        style={{
+                            display: 'flex',
+                            gap: '10px',
+                            color: `${Colors.character_light_title_85}`,
+                        }}
+                    >
+                        <SettingOutlined />
+                        Настройки
+                    </Link>
+                ) : xs ? (
+                    <Button
+                        style={{
+                            fontSize: 14,
+                            width: 32,
+                            height: 32,
+                            borderRadius: '50%',
+                            border: `1px solid ${Colors.character_light_border}`,
+                            background: `${Colors.character_light_sider_background}`,
+                            padding: '7px 8px',
+                        }}
+                        type='text'
+                        icon={<SettingOutlined />}
+                    />
+                ) : (
+                    <Link style={{ color: `${Colors.character_light_title_85}` }}>Настройки</Link>
+                )}
+            </div>
         </header>
     );
 };
